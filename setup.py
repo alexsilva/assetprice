88a2e18c7b9c2cfc592220c7ffb114a111d06637
alexsilva <geniofuturo@gmail.com>
2023-04-25 18:38:08 -0300

    Setup e pacote inicial.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='b3irpf',
+	version='1.0.0',
+	packages=['assetprice'],
+	url='https://github.com/alexsilva/django-assetprice',
+	license='MIT',
+	author='alex',
+	author_email='',
+	description='Pacote que define comandos para cálculo de preço justo'
+)
