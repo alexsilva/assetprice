991b2fb2ad11f7f2d21d9d976a84e1978dc3bdab
alexsilva <geniofuturo@gmail.com>
2023-05-02 21:24:54 -0300

    Salvando o histórico de dividendos pagos.

--- assetprice/management/commands/bazin.py
+++ assetprice/management/commands/bazin.py
@@ -1,12 +1,15 @@
 import statistics
 from decimal import Decimal
 
+from django.utils.timezone import now
+
 from assetprice import settings
-from ._driver import BaseWebDriverCommand, ResponseResult
+from . import paid_history
+from ._driver import ResponseResult
 from ...utils import SearchUrl, EarningUrl
 
 
-class Command(BaseWebDriverCommand):
+class Command(paid_history.Command):
 	"""O comando calcula o preço teto com base na fórmula do Décio Bazin"""
 
 	@classmethod
@@ -19,7 +22,12 @@ class Command(BaseWebDriverCommand):
 	@classmethod
 	def get_max_price(cls, response: ResponseResult):
 		yearly = response.data['assetEarningsYearlyModels']
-		avg = statistics.mean([item['value'] for item in yearly])
+		price, avg = cls._get_max_price([item['value'] for item in yearly])
+		return price, avg
+
+	@classmethod
+	def _get_max_price(cls, values):
+		avg = statistics.mean([value for value in values])
 		price = Decimal(avg) * settings.BAZIN_TAX
 		return price, avg
 
@@ -27,20 +35,26 @@ class Command(BaseWebDriverCommand):
 	def get_url(url, payload):
 		return url + "?" + payload.data
 
-	def get_spec(self, **options):
+	def get_spec(self, ticker, **options):
 		"""Extra e calcula o preço teto"""
-		ticker = options['ticker']
-
 		response = self.get_json(str(SearchUrl(ticker)))
 		if options['verbosity'] > 2:
 			print(response)
 		price = self.get_price(response)
 
-		response = self.get_json(str(EarningUrl(ticker)))
-		if options['verbosity'] > 2:
-			print(response)
+		interval = 5
+		date_now = now()
+		queryset = self.get_from_history(ticker, date_now.year - interval, date_now.year)
+		if queryset.count() >= interval:
+			max_price, avg = self._get_max_price([item.paid for item in queryset])
+		else:
+			response = self.get_json(str(EarningUrl(ticker)))
+			if options['verbosity'] > 2:
+				print(response)
 
-		max_price, avg = self.get_max_price(response)
+			max_price, avg = self.get_max_price(response)
+			if max_price > 0:
+				self.save_history(ticker, response, **options)
 
 		data = {
 			'price': price,
@@ -52,10 +66,10 @@ class Command(BaseWebDriverCommand):
 
 	def handle(self, *args, **options):
 		""""""
-		ticker = options['ticker']
+		ticker = options.pop('ticker')
 		print("Código: ", ticker, file=self.stdout)
 
-		data = self.get_spec(**options)
+		data = self.get_spec(ticker, **options)
 
 		price = data['price']
 		max_price = data['max_price']
---
+++ assetprice/management/commands/paid_history.py
@@ -0,0 +1,40 @@
+from assetprice.management.commands._driver import BaseWebDriverCommand, ResponseResult
+from assetprice.models import AssetEarningHistory
+from assetprice.utils import EarningUrl
+
+
+class Command(BaseWebDriverCommand):
+	help = "Salva o histórico de dividendos em anos"
+	history_model = AssetEarningHistory
+
+	def save_history(self, ticker: str, response: ResponseResult, **options):
+		verbosity = options.get('verbosity', 0)
+		for item in response.data['assetEarningsYearlyModels']:
+			if verbosity > 1:
+				print(item)
+
+			defaults = {'paid': item['value']}
+			instance, _ = self.history_model.objects.get_or_create(
+				defaults=defaults,
+				year=item['rank'],
+				ticker=ticker.upper()
+			)
+			if verbosity > 1:
+				print(instance)
+
+	def get_from_history(self, ticker, start, end):
+		results = self.history_model.objects.filter(
+			ticker__iexact=ticker,
+			year__gte=start,
+			year__lte=end)
+		return results
+
+	def handle(self, *args, **options):
+		ticker = options.pop('ticker')
+		response = self.get_json(str(EarningUrl(ticker)))
+
+		if options['verbosity'] > 2:
+			print(response.data)
+
+		if response.data:
+			self.save_history(ticker, response, **options)
