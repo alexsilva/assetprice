c3551c6ea1058d42f9b972ee14a5c6296a50f7f3
alexsilva <geniofuturo@gmail.com>
2023-05-03 19:31:18 -0300

    Título do app.

--- assetprice/apps.py
+++ assetprice/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AssetPriceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'assetprice'
+
+    verbose_name = "Preço justo"
