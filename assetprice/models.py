667843a6db60d4248efa98ff05d3989d2536bad6
alexsilva <geniofuturo@gmail.com>
2023-05-02 21:19:35 -0300

    Adição de modelo do historico de dividendos pago.

--- assetprice/models.py
+++ assetprice/models.py
@@ -1,3 +1,18 @@
+from decimal import Decimal
+
 from django.db import models
 
-# Create your models here.
+
+class AssetEarningHistory(models.Model):
+	ticker = models.CharField(verbose_name="Código", max_length=8)
+	year = models.PositiveIntegerField(verbose_name="Ano")
+	paid = models.DecimalField(verbose_name="Valor pago",
+	                           max_digits=28,
+	                           decimal_places=4,
+	                           default=Decimal(0))
+
+	class Meta:
+		verbose_name = "Histórico de proventos"
+
+	def __str__(self):
+		return f"{self.ticker}: {self.year} R$ {self.paid}"
