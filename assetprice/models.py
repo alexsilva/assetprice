5a857f488c237a91937e34fe795cddb491a6fe40
alexsilva <geniofuturo@gmail.com>
2023-05-03 22:36:14 -0300

    Alteração do label.

--- assetprice/models.py
+++ assetprice/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class AssetEarningHistory(models.Model):
 	ticker = models.CharField(verbose_name="Código", max_length=8)
 	year = models.PositiveIntegerField(verbose_name="Ano")
-	paid = models.DecimalField(verbose_name="Valor pago",
+	paid = models.DecimalField(verbose_name="Rendimento",
 	                           max_digits=28,
 	                           decimal_places=8,
 	                           default=Decimal(0))
