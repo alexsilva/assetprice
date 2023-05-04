bc5ab293da87ad5891fb210c4007833c96c23f82
alexsilva <geniofuturo@gmail.com>
2023-05-03 21:29:41 -0300

    Implementado plugin que faz o agrupamaneto dos resultados.

--- assetprice/adminx.py
+++ assetprice/adminx.py
@@ -1,12 +1,17 @@
+
 from xadmin.sites import site
+from xadmin.views import ListAdminView
 
 from assetprice.models import AssetEarningHistory
+from assetprice.plugins import ListHistoryGroupAdmin
 
 
 class AssetEarningHistoryAdmin:
 	list_display = ("ticker", "paid", "year")
 	search_fields = ("ticker", "year")
 	list_filter = ("year",)
+	list_history_grouped = True
 
 
 site.register(AssetEarningHistory, AssetEarningHistoryAdmin)
+site.register_plugin(ListHistoryGroupAdmin, ListAdminView)
---
+++ assetprice/plugins.py
@@ -0,0 +1,57 @@
+from django.db.models import Sum, Count
+from xadmin.views import BaseAdminPlugin
+
+
+class ListHistoryGroupAdmin(BaseAdminPlugin):
+	"""Plugin que agrupa os resultados por ativos"""
+	list_history_grouped = False
+
+	def init_request(self, *args, **kwargs):
+		return self.list_history_grouped
+
+	def setup(self, *args, **kwargs):
+		self.index = 1
+		self.ordering_map = {
+			'paid': "paid__sum",
+			'year': "year__count"
+		}
+
+	def queryset(self, qs):
+		qs = qs.values("ticker").distinct().order_by()
+		qs = qs.annotate(Sum("paid"), Count("year"))
+		return qs
+
+	def get_ordering(self, ordering):
+		exclude = ('pk', '-pk')
+		for field_name in exclude:
+			try:
+				ordering.remove(field_name)
+			except ValueError:
+				continue
+		ordering_fields = list(ordering)
+		for field_name in ordering_fields:
+			direction, field_name = field_name[0], field_name[1:]
+			full_name = direction + field_name
+			ordering.remove(full_name)
+			if direction == '-':
+				ordering.append(direction + self.ordering_map.get(field_name, field_name))
+			else:
+				ordering.append(self.ordering_map.get(full_name, full_name))
+		return ordering
+
+	def result_row(self, __, obj):
+		if isinstance(obj, self.model):
+			return __()
+
+		obj = obj.copy()
+
+		obj[self.opts.pk.attname] = self.index
+
+		self.index += 1
+
+		obj['year'] = obj.pop(self.ordering_map['year'])
+		obj['paid'] = obj.pop(self.ordering_map['paid'])
+
+		obj = self.model(**obj)
+
+		return self.admin_view.result_row(obj)
