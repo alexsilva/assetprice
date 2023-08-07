9d8077a05358a322065a613f1fe5a60cde720f05
alexsilva <geniofuturo@gmail.com>
2023-08-07 16:06:04 -0300

    Fix import fail to 'DEFAULT_EXECUTABLE_PATH'.

--- assetprice/settings.py
+++ assetprice/settings.py
@@ -1,13 +1,6 @@
 from django.conf import settings
 from decimal import Decimal
 
-from selenium.webdriver.chrome.service import DEFAULT_EXECUTABLE_PATH
-
-SELENIUM_CHROME_EXECUTABLE_PATH = getattr(settings, "SELENIUM_CHROME_EXECUTABLE_PATH", None)
-
-if SELENIUM_CHROME_EXECUTABLE_PATH is None:
-	SELENIUM_CHROME_EXECUTABLE_PATH = DEFAULT_EXECUTABLE_PATH
-
 API_URL = settings.ENV.str("API_URL", default=None)
 
 SEARCH_URL = API_URL + "/home/mainsearchquery"
@@ -17,3 +10,8 @@ BAZIN_TAX = Decimal(6)
 BAZIN_TAX = Decimal(100) / BAZIN_TAX
 
 TICKER_VALIDATOR = getattr(settings, "TICKER_VALIDATOR", None)
+
+SELENIUM_CHROME_EXECUTABLE_PATH = getattr(settings, "SELENIUM_CHROME_EXECUTABLE_PATH", None)
+
+if SELENIUM_CHROME_EXECUTABLE_PATH is None:
+	SELENIUM_CHROME_EXECUTABLE_PATH = "chromedriver"
