cd09af3dcad7d5533e7d487361815c9bacc28f06
alexsilva <geniofuturo@gmail.com>
2023-08-07 16:12:53 -0300

    Import service.

--- assetprice/management/commands/_driver.py
+++ assetprice/management/commands/_driver.py
@@ -3,6 +3,7 @@ import json
 from django.core.management import BaseCommand
 from django.utils.module_loading import import_string
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
 
