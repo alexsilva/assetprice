7d359c4576edf0e80ecbb7660a00ba897e7d3917
alexsilva <geniofuturo@gmail.com>
2024-03-19 11:49:52 -0300

    Correção de para falha inicial de migrações.

--- assetprice/settings.py
+++ assetprice/settings.py
@@ -1,7 +1,12 @@
 from django.conf import settings
 from decimal import Decimal
+import warnings
 
 API_URL = settings.ENV.str("API_URL", default=None)
+
+if API_URL is None:
+	warnings.warn("App assetprice iniciando sem configuração de 'API_URL'...", UserWarning)
+	API_URL = ''
 
 SEARCH_URL = API_URL + "/home/mainsearchquery"
 EARNING_URL = API_URL + "/acao/companytickerprovents"
